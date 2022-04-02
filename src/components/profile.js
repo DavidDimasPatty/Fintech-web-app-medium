1b576ec830c668fdfe52cfe0ac00a7085198e2a8
michaelmichio <michaelmichio.work@gmail.com>
2022-04-02 23:01:00 +0700

    commit dan update code sama tampilan

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,145 +1,104 @@
-import React from 'react'
-import {useState,useEffect} from 'react'
+import React, {useState, useEffect} from "react";
+import {ReactSession} from "react-client-session";
+import {useHistory, useParams} from "react-router-dom";
 import axios from "axios";
-import {Link} from "react-router-dom";
-import { useHistory,useParams} from 'react-router-dom';
-import "./profile.css"
-import { MDBContainer, MDBRow, MDBCol,
-  MDBCard,MDBBtn } from "mdbreact";
-
-import { ReactSession } from 'react-client-session';
-import { Button } from 'react-bulma-components';
+import "./profile.css";
 
 const Profile = () => {
-    const [name, setName]=useState('');
-    const [email, setEmail]=useState('');
-    const [birth, setBirth]=useState('');
-    const [country, setCountry]=useState('');
-    const [filename, setfilename]=useState(''); 
-    const [video, setvideo]=useState(''); 
-    const [passnum, setpassnum]=useState(''); 
-    const [occupation, setoccupation]=useState(''); 
-    const [address, setaddress]=useState(''); 
-    const [phone, setphone]=useState('');  
-    const [status, setstatus]=useState('');  
-    const {id}=useParams();
-   
-    const history=useHistory();
-    useEffect(()=>{
-        getProfile();
-    },[]);
-
-    function download(links){
-      const link = document.createElement('a');
-      link.href = links;
-      link.target="__blank"
-      document.body.appendChild(link);
-      link.click();
-      document.body.removeChild(link);
-    }
-    
-    
   
-    
-
-    const getProfile = async ()=>{
-    const devEnv=process.env.NODE_ENV !== "production";
-    const {REACT_APP_DEV_URL,REACT_APP_PROD_URL} =process.env;
-        const response= await axios.get(`${devEnv  ? REACT_APP_DEV_URL : REACT_APP_PROD_URL}/customer/${id}`,)
-        console.log(response.status);
-        console.log(response.data);
-        setName(response.data.name);
-        setEmail(response.data.email);
-        setBirth(response.data.birth);
-        setCountry(response.data.country);
-         setfilename(response.data.filename); 
-        setvideo(response.data.videourl); 
-        setpassnum(response.data.passnum); 
-        setoccupation(response.data.occupation); 
-        setaddress(response.data.address); 
-        setphone(response.data.phone);  
-        setstatus(response.data.status);  
-       
-    }
-    const token=ReactSession.get("login");
-    console.log(token);
+  const history = useHistory();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [birth, setBirth] = useState("");
+  const [country, setCountry] = useState("");
+  const [filename, setfilename] = useState("");
+  const [video, setvideo] = useState("");
+  const [passnum, setpassnum] = useState("");
+  const [occupation, setoccupation] = useState("");
+  const [address, setaddress] = useState("");
+  const [phone, setphone] = useState("");
+  const [status, setstatus] = useState("");
+  const {id} = useParams();
   
-    if(token!="true"){
-        history.push('/')
-        return(<div style={'height:100'}></div>);
-    }
+  useEffect(() => {
+    getProfile();
+  }, []);
+  
+  function download(links) {
+    const link = document.createElement("a");
+    link.href = links;
+    link.target = "__blank"
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
+  
+  const getProfile = async () => {
+    const devEnv = process.env.NODE_ENV !== "production";
+    const {REACT_APP_DEV_URL, REACT_APP_PROD_URL} = process.env;
+    const response = await axios.get(`${devEnv ? REACT_APP_DEV_URL : REACT_APP_PROD_URL}/customer/${id}`,)
+    // console.log(response.status);
+    // console.log(response.data);
+    setName(response.data.name);
+    setEmail(response.data.email);
+    setBirth(response.data.birth);
+    setCountry(response.data.country);
+    setfilename(response.data.filename);
+    setvideo(response.data.videourl);
+    setpassnum(response.data.passnum);
+    setoccupation(response.data.occupation);
+    setaddress(response.data.address);
+    setphone(response.data.phone);
+    setstatus(response.data.status);  
+  }
+  
+  const token = ReactSession.get("login");
+  // console.log(token);
+  
+  if(token != "true") {
+    history.push("/");
+    return(
+      <div style={"height:100"}></div>
+    );
+  }
+  
   return (
-    <div class="container column is-20" >
-     <nav class="breadcrumb" aria-label="breadcrumbs">
-  <ul>
-    <li><Link to={`/home`}>Home</Link></li>
-
-    <li><Link to={`/customers`}>Customer List</Link></li>
-    <li><Link to={`/customers/${id}`}>Detail</Link></li>
-    </ul>
-    </nav>
-    <MDBCard className='mt-5 column is-10'>
-      <MDBCol>
-      <MDBRow className='mb-2'>
-        <center><MDBCol> <img src="https://www.kindpng.com/picc/m/451-4517876_default-profile-hd-png-download.png" width={90} height={100}></img></MDBCol></center>
-      </MDBRow>
-      <MDBRow>
-      <center> <MDBCol><h3 className='is-size-2'>{name}</h3></MDBCol></center>
-      </MDBRow>
-      <MDBRow>
-      <center>  <MDBCol><h3 className='is-size-2'>{email}</h3></MDBCol></center>
-      </MDBRow>
-     
-      <MDBRow>
-      <center>  <MDBCol>{birth}</MDBCol></center>
-      </MDBRow>
-
-
-      <MDBRow>
-      <center>  <MDBCol>{country}</MDBCol></center>
-      </MDBRow>
-
-      <MDBRow>
-      <center>  <MDBCol> {passnum}</MDBCol></center>
-      </MDBRow>
-      <MDBRow>
-      <center>  <MDBCol> {phone}</MDBCol></center>
-      </MDBRow>
-      <MDBRow>
-      <center>  <MDBCol> {address}</MDBCol></center>
-      </MDBRow>
-      <MDBRow>
-      <center>  <MDBCol> {occupation}</MDBCol></center>
-      </MDBRow>
-      <MDBRow className='pb-3'>
-      <center>  <MDBCol>Status: {status}</MDBCol></center>
-      </MDBRow>
-
-      </MDBCol>
-      <center>
-      <MDBRow>
-      <MDBCol> 
-      Passport PDF : 
-      </MDBCol>
-      <MDBCol> 
-      <Button color="info  is-small" onClick={() => { download(filename) }}>Download Passport</Button>
-      </MDBCol>
-      </MDBRow>
-      </center>
-      <center>
-      <MDBRow>
-      <MDBCol> 
-      Video 5 sec of customer : 
-      </MDBCol>
-      <MDBCol> 
-      <Button color="info  is-small"  onClick={() => { download(video) }}>Download Video</Button>
-      </MDBCol>
-      </MDBRow>
-      </center>
-      </MDBCard>
+  
+    <div className="container column is-20">
       
-  </div>
+      <nav className="breadcrumb" aria-label="breadcrumbs">
+        <ul>
+          <li><a href="/home">Home</a></li>
+          <li><a href="/customers">Customer List</a></li>
+          <li><a href={`/customers/${id}`}>Detail</a></li>
+        </ul>
+      </nav>
+
+      <div className="cardContainer mt-5 column is-10">
+        
+        <center>
+          <img className="profileImage" src="https://freepikpsd.com/file/2019/10/default-profile-image-png-1-Transparent-Images.png"/>
+          <div className="is-size-2">{name}</div>
+          <div className="is-size-2">{email}</div>
+          <div className="is-size-2">{birth}</div>
+          <div className="is-size-2">{country}</div>
+          <div className="is-size-2">{passnum}</div>
+          <div className="is-size-2">{phone}</div>
+          <div className="is-size-2">{address}</div>
+          <div className="is-size-2">{occupation}</div>
+          <div className="is-size-2">Status: {status}</div>
+          <div className="is-size-2">Passport PDF:</div>
+          <button color="info is-small" onClick={() => { download(filename) }}>Download Passport</button>
+          <div className="is-size-2">Video 5 sec of customer:</div>
+          <button color="info is-small" onClick={() => { download(video) }}>Download Video</button>
+        </center>
+        
+      </div>
+    
+    </div>
+    
   )
+
 }
 
-export default Profile
+export default Profile;
